8fa60db09507fb924fa62f6bb64f05b4711919c9
Curt <Cphilpot798@gmail.com>
2019-04-26 06:43:01 -0400

    Lab Four

---
+++ Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Four
+{
+    class Math
+    {
+        public static int SquaredBoy(int numb)
+        {
+            int Squared = numb * numb;
+            return Squared;
+        }
+
+        public static int CubedUp(int numb)
+        {
+            int Cubed = numb * numb * numb;
+            return Cubed;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Four
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int Wave = 1;
+            while (Wave == 1)
+            {
+                Console.WriteLine("Welcome mathematician! Enter Integer:");
+                int i = 0;
+                int X = int.Parse(Console.ReadLine());
+
+
+                for (i = 1; i <= X; i++)
+                {
+                    int Y = Math.SquaredBoy(i);
+                    int Z = Math.CubedUp(i);
+                    Console.WriteLine($"Number{i}:   Squared:{Y}    Cubed:{Z}");
+
+                }
+                Console.WriteLine("Continue? Yes or No");
+                Wave++;
+                string answer = null;
+                answer = Console.ReadLine();
+                if (answer == "Yes")
+                {
+                    Wave--;
+                }
+                if (answer == "No")
+                {
+                    Console.WriteLine("Hope You Have A Good Day!");
+                }
+
+            }
+
+        }
+    }
+        
+}
